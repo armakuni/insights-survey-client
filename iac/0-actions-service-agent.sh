3cd27fa2f24365381465a8b71882c006c3596ab0
Andrew Gibson <andrew.gibson@armakuni.com>
2022-05-04 17:34:26 +0100

    iac

--- iac/0-actions-service-agent.sh
+++ iac/0-actions-service-agent.sh
@@ -1,2 +1,18 @@
-gcloud iam service-accounts create --project ak-insights insights-sc-pipeline --description "Service accout to run GitHub Action deploying the site"
-gcloud iam service-accounts keys create ./sa-private-key.json --iam-account  insights-sc-pipeline@ak-insights.iam.gserviceaccount.com
+gcloud iam service-accounts create \
+    --project ak-insights \
+    insights-sc-pipeline \
+    --description "Service accout to run GitHub Action deploying the site"
+
+gcloud iam roles create \
+    Custom_insights_sc_pipeline \
+    --project ak-insights \
+    --permissions storage.buckets.get,storage.buckets.list,storage.objects.create,storage.objects.delete,storage.objects.get,storage.objects.list,storage.objects.update \
+    --description "Role for service account to run GitHub Actions deploying the site"
+
+gcloud projects add-iam-policy-binding ak-insights \
+    --member=serviceAccount:insights-sc-pipeline@ak-insights.iam.gserviceaccount.com \
+    --role=projects/ak-insights/roles/Custom_insights_sc_pipeline
+
+gcloud iam service-accounts keys create \
+    ./sa-private-key.json \
+    --iam-account insights-sc-pipeline@ak-insights.iam.gserviceaccount.com
